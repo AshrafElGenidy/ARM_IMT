67add804ee505e7bcc7604a7a56cafa00dd00adc
AshrafElGenidy <ashrafe.elgenidy@gmail.com>
2023-08-18 20:01:03 +0300

    GPIO defines correction

--- COTS/MCAL/GPIO/GPIO_Private.h
+++ COTS/MCAL/GPIO/GPIO_Private.h
@@ -16,16 +16,15 @@
 #define GPIOB       ((volatile u32*)0x40020400)
 #define GPIOC       ((volatile u32*)0x40020800)
 
-#define MODER               0
-#define OTYPER              1
-#define OSPEEDR             2
-#define PUPDR               3
-#define IDR                 4
-#define ODR                 5
-#define BSRR                6
-#define LCKR                7
-#define AFRL                8
-#define AFRH                9
-
+#define MODER               (0u)
+#define OTYPER              (1u)
+#define OSPEEDR             (2u)
+#define PUPDR               (3u)
+#define IDR                 (4u)
+#define ODR                 (5u)
+#define BSRR                (6u)
+#define LCKR                (7u)
+#define AFRL                (8u)
+#define AFRH                (9u)
 
 #endif  /*GPIO_PRIVATE_H_*/
