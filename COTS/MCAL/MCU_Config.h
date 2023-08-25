47e7c11bcf7957dc35e2f98d0512fc58657cae8d
AshrafElGenidy <ashrafe.elgenidy@gmail.com>
2023-08-25 09:55:01 +0300

    MCU Config reordered

--- COTS/MCAL/MCU_Config.h
+++ COTS/MCAL/MCU_Config.h
@@ -6,13 +6,12 @@
 /* Description  : SWC for Configurations affecting multiple modules		*/
 /************************************************************************/
 
+/*Number of Groups and subgroups in interrupt priorities: Options:- GP16_SGP01, GP08_SGP02, GP04_SGP04, GP02_SGP08, GP01_SGP16*/
+#define PRIORITY_TYPE		GP16_SGP01	
+
+/*DO NOT CHANGE THE VALUES BELLOW THIS COMMENT*/
 #define GP16_SGP01		(0u)
 #define GP08_SGP02		(1u)
 #define GP04_SGP04		(2u)
 #define GP02_SGP08		(3u)
 #define GP01_SGP16		(4u)
-
-/*Number of Groups and subgroups in interrupt priorities: Options:- GP16_SGP01, GP08_SGP02, GP04_SGP04, GP02_SGP08, GP01_SGP16*/
-#define PRIORITY_TYPE		GP16_SGP01	
-
-
